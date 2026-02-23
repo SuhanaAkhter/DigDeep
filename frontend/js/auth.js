00e5c7ac4f12c79d369e470499f3b2a89ad4a6b2
Suhana S. Akhter <suhanaakhter922@gmail.com>
2026-02-23 12:53:32 -0500

    SE-01, SE-02, SE-07 -- worked on coach-view pages

---
+++ frontend/js/auth.js
@@ -0,0 +1,34 @@
+// Function to handle Signup Redirects
+const signupForm = document.getElementById('signupForm');
+
+if (signupForm) {
+    signupForm.addEventListener('submit', function (e) {
+        e.preventDefault(); // Stops the page from refreshing
+
+        // 1. Grab the selected role from the dropdown
+        const role = document.getElementById('role').value;
+
+        // 2. Logic to redirect based on the role
+        if (role === 'coach') {
+            window.location.href = "coach-dashboard.html";
+        } else if (role === 'player') {
+            window.location.href = "player-dashboard.html";
+        } else {
+            // This runs if they click the arrow without picking a role
+            alert("please select a role first!");
+        }
+    });
+}
+
+// Function to handle Login Redirects
+const loginForm = document.getElementById('loginForm');
+
+if (loginForm) {
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        
+        // For now, let's assume login goes to the player dashboard 
+        // until you add actual user authentication.
+        window.location.href = "player-dashboard.html";
+    });
+}
